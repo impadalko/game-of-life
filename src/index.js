bb3f0ed91996a13393ae1403498bf9c1eaa39567
Ivan Padalko <dev@impadalko.com>
2021-06-05 20:58:33 -0300

    Allow for setting cells with drag
    Also add placeholder for column and row inputs and separate the code
    better into subfunctions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
-let columns = 0
-let rows = 0
+let columns = 20
+let rows = 20
 let board
+
+let isDrawing = false
+let isErasing = false
+
+window.addEventListener('mousedown', () => {
+  isDrawing = true
+})
+
+window.addEventListener('mouseup', () => {
+  isDrawing = false
+})
+
+const eraseCheckbox = document.getElementById('erase-checkbox')
+eraseCheckbox.addEventListener('input', (e) => {
+  isErasing = e.target.checked
+})
 
 const createBoard = () => {
   return Array.from(Array(rows), () => new Array(columns).fill(false))
@@ -10,11 +26,10 @@ const createGrid = (event) => {
   const table = document.getElementById('table')
   table.innerHTML = ''
 
-  columns = parseInt(event.target[0].value)
-  rows = parseInt(event.target[1].value)
-  if (event.target[2].value) {
+  if (event.target[0].value) columns = parseInt(event.target[0].value)
+  if (event.target[1].value) rows = parseInt(event.target[1].value)
+  if (event.target[2].value)
     document.querySelector(':root').style.setProperty('--grid-size', event.target[2].value)
-  }
   board = createBoard()
 
   for (let i = 0; i < rows; i++) {
@@ -26,6 +41,16 @@ const createGrid = (event) => {
         e.target.classList.toggle('selected')
         board[i][j] = !board[i][j]
       })
+      cell.addEventListener('mousemove', (e) => {
+        if (!isDrawing) return
+        if (isErasing) {
+          e.target.classList.remove('selected')
+          board[i][j] = false
+        } else {
+          e.target.classList.add('selected')
+          board[i][j] = true
+        }
+      })
     }
   }
   event.preventDefault()
@@ -33,6 +58,18 @@ const createGrid = (event) => {
 
 const gridForm = document.getElementById('grid-form')
 gridForm.addEventListener('submit', createGrid)
+
+const setCellFalse = (board, i, j) => {
+  board[i][j] = false
+  const cell = document.getElementById(`${i}-${j}`)
+  cell.classList.remove('selected')
+}
+
+const setCellTrue = (board, i, j) => {
+  board[i][j] = true
+  const cell = document.getElementById(`${i}-${j}`)
+  cell.classList.add('selected')
+}
 
 const countNeighbors = (row, column) => {
   const x = [-1, 0, 1]
@@ -48,21 +85,14 @@ const countNeighbors = (row, column) => {
 }
 
 const frameAdvance = () => {
+  if (!board) return
   const newBoard = createBoard()
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < columns; j++) {
       const count = countNeighbors(i, j)
-      if (count == 3) {
-        newBoard[i][j] = true
-        const cell = document.getElementById(`${i}-${j}`)
-        cell.classList.add('selected')
-      } else if (count == 2) {
-        newBoard[i][j] = board[i][j]
-      } else {
-        newBoard[i][j] = false
-        const cell = document.getElementById(`${i}-${j}`)
-        cell.classList.remove('selected')
-      }
+      if (count == 3) setCellTrue(newBoard, i, j)
+      else if (count == 2) newBoard[i][j] = board[i][j]
+      else setCellFalse(newBoard, i, j)
     }
   }
   board = newBoard
@@ -71,32 +101,28 @@ const frameAdvance = () => {
 const advanceButton = document.getElementById('advance-button')
 advanceButton.addEventListener('click', frameAdvance)
 
-let interval = null
-
 const playButton = document.getElementById('play-button')
+
+let interval = null
+const play = () => {
+  interval = window.setInterval(frameAdvance, 250)
+  playButton.innerHTML = 'Pause'
+}
+const pause = () => {
+  window.clearInterval(interval)
+  interval = null
+  playButton.innerHTML = 'Play'
+}
+
 playButton.addEventListener('click', () => {
-  if (!interval) {
-    interval = window.setInterval(frameAdvance, 250)
-    event.target.innerHTML = 'Pause'
-  } else {
-    window.clearInterval(interval)
-    interval = null
-    event.target.innerHTML = 'Play'
-  }
+  if (!board) return
+  if (!interval) play()
+  else pause()
 })
 
 const clearButton = document.getElementById('clear-button')
 clearButton.addEventListener('click', () => {
-  if (interval) {
-    window.clearInterval(interval)
-    interval = null
-    playButton.innerHTML = 'Play'
-  }
-  for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < columns; j++) {
-      const cell = document.getElementById(`${i}-${j}`)
-      cell.classList.remove('selected')
-    }
-  }
-  board = createBoard()
+  if (!board) return
+  if (interval) pause()
+  for (let i = 0; i < rows; i++) for (let j = 0; j < columns; j++) setCellFalse(board, i, j)
 })
